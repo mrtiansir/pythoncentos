752defa54d515a5c20a71efca92277b100f3b3ce
mrtiansir <657036136@qq.com>
2020-01-04 19:59:01 +0800

    commit 2

--- test_kamene.py
+++ test_kamene.py
@@ -6,8 +6,6 @@ import logging
 logging.getLogger("kamene.runtime").setLevel(logging.ERROR)
 from kamene.all import *
 
-ping_pkt = IP(dst='8.8.8.8')/ICMP()
-ping_result = sr1(ping_pkt, timeout=2, verbose=False)
-
+ping_result = sr1(IP(dst='8.8.8.8')/ICMP(), timeout=2, verbose=False)
 ping_result.show()
 
