674b0b138f35a19e4ff83b112b8fdaaf0bdd53c4
mrtiansir <657036136@qq.com>
2020-01-20 10:47:08 +0800

    commit 21

--- chapter_13_multi_process/class_13_ping_scan.py
+++ chapter_13_multi_process/class_13_ping_scan.py
@@ -2,11 +2,13 @@
 # _*_ coding=utf-8 _*_
 # python basic scripts
 
-from chapter_13_multi_process.class_13_ping_one import my_ping
-from multiprocessing.pool import ThreadPool
-import ipaddress
+
 
 def ping_scan(network):
+    from chapter_13_multi_process.class_13_ping_one import my_ping
+    from multiprocessing.pool import ThreadPool
+    import ipaddress
+
     pool = ThreadPool(processes=150)
     net = ipaddress.ip_network(network)
 
@@ -28,18 +30,28 @@ def ping_scan(network):
     return active_ip
 
 if __name__ == '__main__':
+    from argparse import ArgumentParser
+    usage = '/usr/bin/python3 class_13_ping_scan.py -n network'
+
+    parser = ArgumentParser(usage=usage)
+
+    parser.add_argument('-n', '--net', dest='network', help='Specify an network for ping scan, eg.192.168.10.0/24', type=str)
+    args = parser.parse_args()
+    print('请稍等，ping扫描正在进行中......')
+    result = ping_scan(args.network)
+
     import pickle
     from datetime import datetime
     filename = 'scan_save_pickle_' + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '.pl'
     dbfile = open(filename, 'wb')
-    scan_net = input('请输入需要扫描的网段，eg. 192.168.1.0/24：')
-    print('请稍等，ping扫描正在进行中......')
-    result = ping_scan(scan_net)
+    # scan_net = input('请输入需要扫描的网段，eg. 192.168.1.0/24：')
     pickle.dump(result, dbfile)
     dbfile.close()
+
+
     dbfile = open(filename, 'rb')
     final = pickle.load(dbfile)
-    print(f'扫描完毕，网段{scan_net}中活动ip如下：')
+    print(f'扫描完毕，网段{args.network}中活动ip如下：')
     print('='*60)
     for final_ip in final:
         print(final_ip)
